4084b2ac3670c88d38c6ea2284bef0ad44f4f7d0
SAHARSH GAURAV <33245691+sgaurav07@users.noreply.github.com>
2019-07-22 22:38:51 +0530

    JSON writing

---
+++ testingJSON.py
@@ -0,0 +1,24 @@
+import json
+
+
+data = {'people': []}
+
+data['people'].append({
+    'name': 'Scott',
+    'website': 'stackabuse.com',
+    'from': 'Nebraska'
+})
+data['people'].append({
+    'name': 'Larry',
+    'website': 'google.com',
+    'from': 'Michigan'
+})
+data['people'].append({
+    'name': 'Tim',
+    'website': 'apple.com',
+    'from': 'Alabama'
+})
+
+
+with open("test.json",'w') as files:
+    json.dump(data,files)
